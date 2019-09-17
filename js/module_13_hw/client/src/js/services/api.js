11e3f099d1f815d8c37ca5c1a37ead56dd36c8a7
Artem2313 <huselnykov@gmail.com>
2019-09-17 13:28:46 +0300

    module-13

---
+++ js/module_13_hw/client/src/js/services/api.js
@@ -0,0 +1,55 @@
+const URL = "http://localhost:3000/notes";
+
+export const getNotes = () => {
+    return fetch(URL).then(responce => {
+      if (responce.ok) {
+        return responce.json();
+      }
+      throw new Error("Error while fetching getNotes" + responce.statusText);
+    });
+  };
+  
+  export const saveNotes = note => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(note)
+    };
+    return fetch(URL, options).then(responce => {
+      if (responce.ok) {
+        return responce.json();
+      }
+      throw new Error("Error while fetching saveNotes" + responce.statusText);
+    });
+  };
+  
+  export const deleteNotes = id => {
+    const options = {
+      method: "DELETE"
+    };
+    return fetch(`${URL}/${id}`, options).then(responce => {
+      if (responce.ok) {
+        return responce.json();
+      }
+      throw new Error("Error while fetching deleteNotes" + responce.statusText);
+    });
+  };
+
+  export const updateNotes = (id, updatedNote) => {
+    const options = {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedNote)
+    };
+    return fetch(`${URL}/${id}`, options).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+
+        throw new Error("Error while fetching update" + responce.statusText);
+    })
+};
